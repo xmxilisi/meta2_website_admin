57f6f1bbb62fe42d44f29cb45f677b34f34369f2
Rohan <55588441@qq.com>
2022-11-14 18:02:27 +0800

    更新目录

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.1.45:8082/`,
+        target: `http://192.168.1.200:8082/`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
